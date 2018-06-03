5dc6b065267fc39808a9501319d8a0983dd1ec6a
tobiasnowen <tobias.nowen@adeprimo.se>
2018-06-03 14:59:09 +0200

    Basic board

--- src/js/board.class.js
+++ src/js/board.class.js
@@ -1,11 +1,16 @@
 class Board {
 
-    constructor () {
-
+    constructor (selector) {
+        this.selector = selector;
     }
 
     render (elements) {
-        
+        const container = $(this.selector);
+        container.empty();
+        elements.forEach((element, index) => {
+            container.append($(`
+                <div class="element-${element}" data-index="${index}"></div>
+            `))
+        })
     }
-
 }
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(
+    function () {
+        const board = new Board('.board');
+        board.render([0, 1, 2, 0, 1, 2, 0, 1, 2,]);
+        board.render([0, 1, 2, 0, 1, 2, 0, 1, 2,]);
+    });
